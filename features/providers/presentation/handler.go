f1112dffdbc8abab8e583b6497cf47c9d7efc10a
Fakhrii Habibillah An Naufal <fha.naufal06@gmail.com>
2021-12-30 14:41:09 +0700

    change providers response

--- features/providers/presentation/handler.go
+++ features/providers/presentation/handler.go
@@ -4,6 +4,7 @@ import (
 	"bayareen-backend/features/providers"
 	provider_request "bayareen-backend/features/providers/presentation/request"
 	provider_response "bayareen-backend/features/providers/presentation/response"
+	"bayareen-backend/helper/response"
 	"net/http"
 	"strconv"
 
@@ -24,57 +25,75 @@ func (ph *ProviderHandler) Create(c echo.Context) error {
 	providerRequest := provider_request.Provider{}
 
 	if err := c.Bind(&providerRequest); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		return c.JSON(http.StatusBadRequest, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
 	resp, err := ph.ProviderBusiness.Create(providerRequest.ToCore())
 
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
-	return c.JSON(http.StatusCreated, map[string]interface{}{
-		"message": "success",
-		"data":    provider_response.FromCore(resp),
+	return c.JSON(http.StatusCreated, response.BasicResponse{
+		Message: "success",
+		Data:    provider_response.FromCore(resp),
 	})
 }
 
 func (ph *ProviderHandler) GetAll(c echo.Context) error {
 	resp := ph.ProviderBusiness.GetAll()
 
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "success",
-		"data":    provider_response.FromCoreSlice(resp),
+	return c.JSON(http.StatusOK, response.BasicResponse{
+		Message: "success",
+		Data:    provider_response.FromCoreSlice(resp),
 	})
 }
 
 func (ph *ProviderHandler) GetById(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		return c.JSON(http.StatusBadRequest, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
 	resp, err := ph.ProviderBusiness.GetById(id)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "success",
-		"data":    provider_response.FromCore(resp),
+	return c.JSON(http.StatusOK, response.BasicResponse{
+		Message: "success",
+		Data:    provider_response.FromCore(resp),
 	})
 }
 
 func (ph *ProviderHandler) Update(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		return c.JSON(http.StatusBadRequest, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
 	providerRequest := provider_request.Provider{}
 
 	if err := c.Bind(&providerRequest); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		return c.JSON(http.StatusBadRequest, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
 	core := providerRequest.ToCore()
@@ -82,7 +101,10 @@ func (ph *ProviderHandler) Update(c echo.Context) error {
 
 	_, err = ph.ProviderBusiness.Update(core)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
 	return c.JSON(http.StatusNoContent, []int{})
@@ -91,11 +113,17 @@ func (ph *ProviderHandler) Update(c echo.Context) error {
 func (ph *ProviderHandler) Delete(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		return c.JSON(http.StatusBadRequest, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
 	if err = ph.ProviderBusiness.Delete(id); err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusInternalServerError, response.BasicResponse{
+			Message: "failed",
+			Data:    err.Error(),
+		})
 	}
 
 	return c.JSON(http.StatusNoContent, []int{})
