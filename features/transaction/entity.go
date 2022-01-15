0b290bd25e1b729e2d709e8f960e0e55ea6334fe
Francisco <fcisco.dev@gmail.com>
2022-01-15 15:45:56 +0700

    add entity for transaction

--- features/transaction/entity.go
+++ features/transaction/entity.go
@@ -1,1 +1,35 @@
 package transaction
+
+import (
+	"bayareen-backend/features/payment_gateway"
+	"time"
+)
+
+type Core struct {
+	Id              int
+	UserId          int
+	ProductId       int
+	InvoiceId       string
+	PaymentMethodId int
+	Status          string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type XenditCallback struct {
+	Id             string
+	TransactionId  string
+	Status         string
+	PaymentMethod  string
+	PaymentChannel string
+}
+
+type Data interface {
+	Create(*Core) (*Core, error)
+	Update(*Core) (*Core, error)
+}
+
+type Business interface {
+	Create(*Core) (*Core, payment_gateway.InvoiceData, error)
+	UpdatePayment(XenditCallback) error
+}
