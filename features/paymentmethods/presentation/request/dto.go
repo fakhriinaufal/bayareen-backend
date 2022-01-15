3077c154b85b46f2808fa335b25de982e9093e2b
Francisco <fcisco.dev@gmail.com>
2022-01-15 15:44:34 +0700

    minor

--- features/paymentmethods/presentation/request/dto.go
+++ features/paymentmethods/presentation/request/dto.go
@@ -1,13 +1,11 @@
 package presentation
 
-import "bayareen-backend/features/paymentmethods"
-
 type PaymentMethod struct {
 	Name string `json:"name"`
 }
 
-func (paymentMethod *PaymentMethod) ToCore() *paymentmethods.Core {
-	return &paymentmethods.Core{
-		Name: paymentMethod.Name,
-	}
-}
+// func (paymentMethod *PaymentMethod) ToCore() *paymentmethods.Core {
+// 	return &paymentmethods.Core{
+// 		Name: paymentMethod.Name,
+// 	}
+// }
