e713ee49ac833f0863fa0cdbffcca5ebc2efcfd2
Francisco <fcisco.dev@gmail.com>
2022-01-15 15:49:43 +0700

    add repository for transaction

--- features/transaction/repository/repository.go
+++ features/transaction/repository/repository.go
@@ -1,1 +1,36 @@
 package repository
+
+import (
+	"bayareen-backend/features/transaction"
+
+	"gorm.io/gorm"
+)
+
+type transactionRepository struct {
+	Conn *gorm.DB
+}
+
+func NewPostgresTransactionRepository(conn *gorm.DB) transaction.Data {
+	return &transactionRepository{
+		Conn: conn,
+	}
+}
+
+func (tr *transactionRepository) Create(data *transaction.Core) (*transaction.Core, error) {
+	record := FromCore(data)
+	if err := tr.Conn.Create(record).Error; err != nil {
+		return &transaction.Core{}, err
+	}
+
+	return record.ToCore(), nil
+}
+
+func (tr *transactionRepository) Update(data *transaction.Core) (*transaction.Core, error) {
+	record := FromCore(data)
+
+	if err := tr.Conn.Updates(record).Error; err != nil {
+		return &transaction.Core{}, err
+	}
+
+	return record.ToCore(), nil
+}
