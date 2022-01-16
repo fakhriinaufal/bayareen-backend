4630ef0ac2602971166435f506ac106e362fc6c4
Francisco <fcisco.dev@gmail.com>
2022-01-16 22:42:44 +0700

    add TransactionProduct response

--- features/transaction/presentation/response/dto.go
+++ features/transaction/presentation/response/dto.go
@@ -1,7 +1,7 @@
 package response
 
 import (
-	"bayareen-backend/features/payment_gateway"
+	"bayareen-backend/features/products/presentation/response"
 	"bayareen-backend/features/transaction"
 )
 
@@ -15,14 +15,44 @@ type TransactionResponse struct {
 	Status          string `json:"status"`
 }
 
-func FromCore(trans *transaction.Core, invoice payment_gateway.InvoiceData) TransactionResponse {
+type TransactionProductResponse struct {
+	Id              int    `json:"id"`
+	UserId          int    `json:"user_id"`
+	ProductId       int    `json:"product_id"`
+	InvoiceId       string `json:"invoice_id"`
+	PaymentMethodId int    `json:"payment_method_id"`
+	Status          string `json:"status"`
+	Product         response.Product
+}
+
+func ToTransactionResponse(trans *transaction.Core) TransactionResponse {
 	return TransactionResponse{
 		Id:              trans.Id,
 		UserId:          trans.UserId,
 		ProductId:       trans.ProductId,
 		InvoiceId:       trans.InvoiceId,
-		InvoiceUrl:      invoice.InvoiceUrl,
+		InvoiceUrl:      trans.InvoiceUrl,
 		PaymentMethodId: trans.PaymentMethodId,
 		Status:          trans.Status,
 	}
+}
+
+func ToTransactionProductResponse(trans *transaction.Core) TransactionProductResponse {
+	return TransactionProductResponse{
+		Id:              trans.Id,
+		UserId:          trans.UserId,
+		ProductId:       trans.ProductId,
+		InvoiceId:       trans.InvoiceId,
+		PaymentMethodId: trans.PaymentMethodId,
+		Status:          trans.Status,
+		Product:         *response.FromCore(&trans.Product),
+	}
+}
+
+func ToTransactionProductsResponse(transactions []transaction.Core) []TransactionProductResponse {
+	conv := []TransactionProductResponse{}
+	for _, val := range transactions {
+		conv = append(conv, ToTransactionProductResponse(&val))
+	}
+	return conv
 }
