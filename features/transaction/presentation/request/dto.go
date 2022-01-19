3addca975845c3db648d16eeaec3f6408bb51934
Francisco <fcisco.dev@gmail.com>
2022-01-19 14:41:36 +0700

    add price and reference id atribute

--- features/transaction/presentation/request/dto.go
+++ features/transaction/presentation/request/dto.go
@@ -5,11 +5,12 @@ import "bayareen-backend/features/transaction"
 type TransactionRequest struct {
 	UserId    int `json:"user_id"`
 	ProductId int `json:"product_id"`
+	Price     int `json:"price"`
 }
 
 type XenditCallback struct {
 	Id             string `json:"id"`
-	TransactionId  string `json:"external_id"`
+	ReferenceId    string `json:"external_id"`
 	Status         string `json:"status"`
 	PaymentMethod  string `json:"payment_method"`
 	PaymentChannel string `json:"payment_channel"`
@@ -19,13 +20,14 @@ func (tr *TransactionRequest) ToCore() *transaction.Core {
 	return &transaction.Core{
 		UserId:    tr.UserId,
 		ProductId: tr.ProductId,
+		Price:     tr.Price,
 	}
 }
 
 func (xc *XenditCallback) ToCore() *transaction.XenditCallback {
 	return &transaction.XenditCallback{
 		Id:             xc.Id,
-		TransactionId:  xc.TransactionId,
+		ReferenceId:    xc.ReferenceId,
 		Status:         xc.Status,
 		PaymentMethod:  xc.PaymentMethod,
 		PaymentChannel: xc.PaymentChannel,
