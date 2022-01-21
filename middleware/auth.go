6c230ffc5367133f321a50c0d74b1d03c9ed19a0
Francisco <fcisco.dev@gmail.com>
2022-01-21 15:48:58 +0700

    take secret parameter on CreateToken func

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,14 +1,13 @@
 package middleware
 
 import (
-	"bayareen-backend/config"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 )
 
-func CreateToken(id int, isAdmin bool) (string, error) {
+func CreateToken(id int, isAdmin bool, secret string) (string, error) {
 	claims := jwt.MapClaims{}
 
 	claims["id"] = id
@@ -17,7 +16,7 @@ func CreateToken(id int, isAdmin bool) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	return token.SignedString([]byte(config.JWT_KEY))
+	return token.SignedString([]byte(secret))
 }
 
 func ExtractClaim(e echo.Context) (claims map[string]interface{}) {
